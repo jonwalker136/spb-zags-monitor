bf28c194bb3501db5a9d787e620ff4753a204db7
jonwalker136 <bogdan0545@gmail.com>
2026-08-04 20:38:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
+import threading
+
+from bot.telegram_bot import run_bot
 from monitor.checker import run_check_loop
 from config import CHECK_INTERVAL
 
 
-if __name__ == "__main__":
-    print("SPB ZAGS Monitor запущен")
-
+def start_monitor():
     run_check_loop(CHECK_INTERVAL)
+
+
+if __name__ == "__main__":
+
+    print("SPB ZAGS Monitor запускается")
+
+    monitor_thread = threading.Thread(
+        target=start_monitor,
+        daemon=True
+    )
+
+    monitor_thread.start()
+
+    run_bot()
