790041d709c5e51d45b890a8850179db0a7a7bbb
jonwalker136 <bogdan0545@gmail.com>
2026-08-04 18:29:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,5 @@
-import asyncio
-from config import BOT_TOKEN
-
-async def main():
-    print("SPB ZAGS Monitor запущен")
-
-    if not BOT_TOKEN:
-        print("Ошибка: не найден BOT_TOKEN")
-        return
-
-    print("Telegram бот настроен")
+from bot.telegram_bot import run_bot
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    run_bot()
