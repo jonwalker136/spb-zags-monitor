98d990f0df90e09f0362a742eeaa926d00df8047
jonwalker136 <bogdan0545@gmail.com>
2026-08-04 21:18:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,7 @@
-import threading
+import asyncio
 
-from bot.telegram_bot import run_bot
-from monitor.checker import run_check_loop
-from config import CHECK_INTERVAL
-
-
-def start_monitor():
-    run_check_loop(CHECK_INTERVAL)
+from bot.telegram_bot import main
 
 
 if __name__ == "__main__":
-
-    print("SPB ZAGS Monitor запускается")
-
-    monitor_thread = threading.Thread(
-        target=start_monitor,
-        daemon=True
-    )
-
-    monitor_thread.start()
-
-    run_bot()
+    asyncio.run(main())
