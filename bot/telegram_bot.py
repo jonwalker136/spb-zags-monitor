04cb275fdec8c31ef4809b519c647b72192189f7
jonwalker136 <bogdan0545@gmail.com>
2026-08-04 21:17:37 +0300

    Update telegram_bot.py

--- bot/telegram_bot.py
+++ bot/telegram_bot.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from telegram import Update
 from telegram.ext import (
     Application,
@@ -9,24 +11,25 @@ from config import BOT_TOKEN
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-
     await update.message.reply_text(
-        "🤖 SPB ZAGS Monitor работает"
+        "🤖 SPB ZAGS Monitor работает!"
     )
 
 
 async def check(update: Update, context: ContextTypes.DEFAULT_TYPE):
-
     await update.message.reply_text(
         "🔎 Проверка запущена"
     )
 
 
-def run_bot():
+async def main():
 
-    app = Application.builder()\
-        .token(BOT_TOKEN)\
+    app = (
+        Application
+        .builder()
+        .token(BOT_TOKEN)
         .build()
+    )
 
     app.add_handler(
         CommandHandler("start", start)
@@ -38,4 +41,12 @@ def run_bot():
 
     print("Telegram бот запущен")
 
-    app.run_polling()
+    await app.initialize()
+    await app.start()
+    await app.updater.start_polling()
+
+    await asyncio.Event().wait()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
